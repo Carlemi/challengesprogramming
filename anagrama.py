fd2c7360aed729be7e8dd66600adb105752c6704
Carlos <carlos.mendoza007@hotmail.com>
2023-04-09 22:11:53 -0500

    iniciando anagrama

---
+++ anagrama.py
@@ -0,0 +1,7 @@
+'''Este ejercicio consiste en crear una función que reciba 
+dos palabras (string) y retorne falso o verdadero según 
+sean o no anagramas'''
+
+def my_anagrama(word_1, word_2):
+    if len(word_1) == len(word_2):
+
