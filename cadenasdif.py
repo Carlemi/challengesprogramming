30645b2498172fb72b96caf93cc842615b328215
Carlos <carlos.mendoza007@hotmail.com>
2023-04-15 15:52:15 -0500

    ejercicio donde se reciben en una función dos cadenas y se imprimen dos cadenas donde ninguna es igual a la otra

---
+++ cadenasdif.py
@@ -0,0 +1,10 @@
+## Vamos a crear una función que reciba dos cadenas de textos, nos retorne dos cadenas pero que en la primera no haya 
+# los caracteres de la segunda y viceversa 
+
+def cadenas(cadena_1 = 'hola', cadena_2 = 'vete'):
+    salida_1 = cadena_1
+    salida_2 = cadena_2
+    print(salida_1, ' ', salida_2)
+
+cadenas('aja', 'no sé')
+
