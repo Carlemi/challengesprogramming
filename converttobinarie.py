ac2792cbdd4b3b04061191ea8c42b218458010d1
Carlos <carlos.mendoza007@hotmail.com>
2023-04-16 18:54:53 -0500

    he descubierto que estaba haciendo mal la lógica del for XD ya optimicé el código

--- converttobinarie.py
+++ converttobinarie.py
@@ -1,42 +1,15 @@
 ## vamos a realizar un programa que reciba un número por parte del usuario y lo convierta a binario
 
-bits_table = [128, 64, 32, 16, 8, 4, 2, 1]
 
-numerocliente = int(input('Sr. Usuario por favor ingrese un número: '))
+def conver_to_binary():
+    bits_table = [128, 64, 32, 16, 8, 4, 2, 1]
+    numerocliente = int(input('Sr. Usuario por favor ingrese un número: '))
+    for numero in bits_table:
+        if numero >= numerocliente:
+            print(1)    
+        else:
+            print(0)
+conver_to_binary()
 
-
-if numerocliente >= bits_table[0]:
-    print(1)
-else:
-    print(0)
-if numerocliente >= bits_table[1]:
-    print(0)
-else:
-    print(0)
-if numerocliente >= bits_table[2]:
-    print(1)
-else:
-    print(0)
-if numerocliente >= bits_table[3]:
-    print(1)
-else:
-    print(0)
-if numerocliente >= bits_table[4]:
-    print(1)
-else: 
-    print(0)
-if numerocliente >= bits_table[5]:
-    print(1)
-else:
-    print(0)
-if numerocliente >= bits_table[6]:
-    print(1)
-else:
-    print(0)
-if numerocliente >= bits_table[7]:
-    print(1)
-else:
-    print(0)
-print(bits_table)    
 
 
